ca050ed2ec6ce27928d3c193eb7d0d10c50677fd
kolbysisk <kolby.sisk@gmail.com>
2013-05-28 02:14:27 -0400

    finished login page html

--- application/views/login.php
+++ application/views/login.php
@@ -5,3 +5,46 @@
 
 		<div class="divider"></div>
 	</section>
+
+	<section id="forms">
+		<section class="sizer">
+			<article>
+				<h1>Login Via Social</h1>
+				<div>
+					<a href="#"></a>
+					<a href="#"></a>
+					<p>We <em>promise</em> we’ll never post anything without your permission.</p>
+				</div>
+			</article>
+
+			<article>
+				<h1>Login with an email</h1>
+				<div>
+					<h2>Email Adress</h2>
+					<input type="text"></input>
+
+					<h2>Password</h2>
+					<input type="text"></input>
+
+					<a href="#">Login</a>
+					<a href="#">Forgot your password?</a>
+				</div>
+			</article>
+
+			<article>
+				<h1>Create an account</h1>
+				<div>
+					<h2>Name</h2>
+					<input type="text"></input>
+
+					<h2>Email</h2>
+					<input type="text"></input>
+
+					<h2>Password</h2>
+					<input type="password"></input>
+
+					<a href="#">Sign Up</a>
+				</div>
+			</article>
+		</section>
+	</section>
